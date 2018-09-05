76090c58a8cfb6f24b69cc1fc487367af6b3acbf
Evgeny <rogue77mev@gmail.com>
2018-09-05 17:11:47 +0500

    class Vector rdy

--- game.js
+++ game.js
@@ -1,1 +1,27 @@
 'use strict';
+
+class Vector{
+  constuctor(x = 0, y = 0){
+    this.x = x;
+    this.y = y;
+  }
+
+  plus(){
+
+  }
+}
+
+// class Actor{
+
+// }
+
+// class Level{
+
+// }
+
+const grid = [
+  new Array(3),
+  ['wall', 'wall', 'lava']
+];
+const level = new Level(grid);
+runLevel(level, DOMDisplay);
